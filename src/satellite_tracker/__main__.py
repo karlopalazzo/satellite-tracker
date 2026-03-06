23d94a8559c12a8eae07ce88a6ca1e08b17516bf
karlopalazzo <karol_palac@o2.pl>
2026-03-06 21:17:51 +0100

    Added SatelliteTracker class.

--- src/satellite_tracker/__main__.py
+++ src/satellite_tracker/__main__.py
@@ -1,33 +1,34 @@
 from datetime import datetime, timezone
 from math import radians, degrees
-from .calc.frames import get_satellite_az_el_range
-from .calc.propagator import propagate_satellite
+from sgp4.api import Satrec
 from .infrastructure.tle_provider import get_satellite_tle
+from .domain.tracker import SatelliteTracker
+
+norad_id = 25544
+line1, line2 = get_satellite_tle(norad_id)
+
+satellite = Satrec.twoline2rv(line1, line2)
 
 # Observer's geodetic coordinates
-observer_lat_rad = radians(51.62773)  # Wroclaw latitude in radians
-observer_lon_rad = radians(15.88198)  # Wroclaw longitude in radians
-observer_alt_m = 126.0  # Observer altitude in meters
+observer_lat = radians(51.62773)  # Wroclaw latitude in radians
+observer_lon = radians(15.88198)  # Wroclaw longitude in radians
+observer_alt = 126.0  # Observer altitude in meters
 
-# satrec: satellite TLE
-line1, line2 = get_satellite_tle(25544)
-print(line1, line2, sep='\n')
-satellite = propagate_satellite(line1, line2)  # Initialize satellite record from TLE lines
-
+tracker = SatelliteTracker(
+    satellite,
+    observer_lat,
+    observer_lon,
+    observer_alt
+)
 
 # Satelite: r_eci:ECI -> r_ecef:ECEF -> observer_ecef:ECEF -> enu:ENU -> az_rad, el_rad, range_m
-az_rad, el_rad, range_m = get_satellite_az_el_range(
-    observer_lat_rad,
-    observer_lon_rad,
-    observer_alt_m,
-    satellite,
-    )
+az, el, rng = tracker.get_az_el_range()
 
-print(f"Azimuth: {degrees(az_rad):.2f} deg, \
-        Elevation: {degrees(el_rad):.2f} deg, \
-        Range: {range_m:.2f} m")
+print(f"Azimuth: {degrees(az):.2f} deg, \
+        Elevation: {degrees(el):.2f} deg, \
+        Range: {rng:.2f} m")
 
-if degrees(el_rad) < 0:
+if tracker.is_visible():
     print("Target under horizon.")
 else:
     print("Target over horizon.")
---
+++ src/satellite_tracker/domain/tracker.py
@@ -0,0 +1,70 @@
+import numpy as np
+from datetime import datetime, timezone
+
+from satellite_tracker.calc.frames import (
+    geodetic_to_ecef,
+    gmst_from_jd,
+    eci_to_ecef
+)
+
+from satellite_tracker.calc.topocentric import (
+    ecef_to_enu,
+    enu_to_az_el_range
+)
+
+from satellite_tracker.calc.propagator import propagate_to_eci
+
+
+class SatelliteTracker:
+    """
+    High-level satellite tracking pipeline.
+    Converts TLE propagation results into azimuth, elevation and range
+    for a given observer's coordinates.
+    """
+
+    def __init__(self, satellite, obs_lat_rad, obs_lon_rad, obs_alt_m):
+        self.satellite = satellite
+        self.obs_lat = obs_lat_rad
+        self.obs_lon = obs_lon_rad
+        self.obs_alt = obs_alt_m
+
+        # Pre-computing observer ECEF
+        self.obs_ecef = np.array(
+            geodetic_to_ecef(obs_lat_rad, obs_lon_rad, obs_alt_m)
+        )
+
+    def get_az_el_range(self, obs_time: datetime | None = None):
+        """
+        Compute satellite azimuth, elevation and range for given time
+        """
+        if obs_time is None:
+            obs_time = datetime.now(timezone.utc)
+        
+        # Pipeline 1: TLE -> ECI propagate satellite
+        r_eci, jd, fr = propagate_to_eci(self.satellite, obs_time)
+
+        # Pipeline 2: GMST
+        gmst_rad = gmst_from_jd(jd, fr)
+
+        # Pipeline 3: Satellite ECI -> ECEF
+        r_ecef = eci_to_ecef(r_eci, gmst_rad)
+
+        # Pipeline 4: Satellite ECEF -> ENU
+        enu = ecef_to_enu(
+            self.obs_ecef,
+            r_ecef,
+            self.obs_lat,
+            self.obs_lon
+        )
+
+        # Pipeline 5: ENU -> Azimuth, elevation, range
+        az_rad, el_rad, range_m = enu_to_az_el_range(enu)
+
+        return az_rad, el_rad, range_m
+
+    def is_visible(self, obs_time: datetime | None = None):
+        """
+        Check whether satellite is over horizon.
+        """
+        _, el_rad, _ = self.get_az_el_range(obs_time)
+        return el_rad > 0
